42b851ac096ebdaad5d2b62c621762633c483985
Min-Thway-Htut <minthwayhtut568@gmail.com>
2025-02-05 20:51:59 +0100

    Another Testing

--- src/components/ImageUploader.jsx
+++ src/components/ImageUploader.jsx
@@ -1,35 +1,11 @@
-import React, { useState } from 'react';
+import React from "react";
 
 const ImageUploader = () => {
-  const [image, setImage] = useState(null);
-  const [fileInput, setFileInput] = useState(null);
-
-  const handleImageChange = () => {
-    if (fileInput) {
-      const file = fileInput.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onloadend = () => {
-          setImage(reader.result);
-        };
-        reader.readAsDataURL(file);
-      }
-    }
-  };
-
   return (
     <div>
-          {image && <img src={image} alt="Uploaded" />}
-      <input
-        type="file"
-        accept="image/*"
-        ref={(fileInputRef) => setFileInput(fileInputRef)}
-        style={{ display: 'none' }}
-        onChange={handleImageChange}
-      />
-      <button onClick={() => fileInput.click()}>Upload Image</button>
+      <button>Upload Image</button>
     </div>
-  );
-};
+  )
+}
 
 export default ImageUploader;
