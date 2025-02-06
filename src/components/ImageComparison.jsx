291144c540865adcf51454e3c56248287a9c6ea0
Min-Thway-Htut <minthwayhtut568@gmail.com>
2025-02-06 12:11:29 +0100

    Another Testing

--- src/components/ImageComparison.jsx
+++ src/components/ImageComparison.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const ImageComparison = () => {
+
+    useEffect(() => {
+        const container = document.querySelector('.container');
+        const slider = document.querySelector('.slider');
+        
+        const handleSliderInput = (e) => {
+            container.style.setProperty('--position', `${e.target.value}%`);
+        };
+
+        slider.addEventListener('input', handleSliderInput);
+
+        return () => {
+            slider.removeEventListener('input', handleSliderInput);
+        };
+    }, []);
     return(
             <main>
                 <div className="container">
                     <div className="image-container">
-                        <img src="/download.jpg" alt="before result"/>
-                        <img src="/download_bw.jpg" alt="after result" />
+                        <img src="/download.jpg" alt="before result" className="image-before slider-image"/>
+                        <img src="/download_bw.jpg" alt="after result" className="image-after slider-image"/>
                     </div>
                     <input type="range" min="0" max="100"
                     value="50" className="slider" aria-label="Percentage of before photo shown"/>
